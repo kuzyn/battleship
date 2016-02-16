d2735c778c432054e41c8b16b928d53341ca81bb
kuzyn <hello@samuelcousin.com>
2016-02-16 01:37:36 +0000

    feat: toggles & css

--- public/js/front-client.js
+++ public/js/front-client.js
@@ -18,20 +18,47 @@
     .fail(function(jqXHR, textStatus, err) {
       console.error(jqXHR.status + ' ' + err + ' ' + jqXHR);
       result = jqXHR.status + ' ' + err;
+      $('.output-raw').html(JSON.stringify(jqXHR, null, 2));
     })
     .always(function() {
       console.info('$.ajax.complete');
       formatGrid(result);
+      $('.output-raw').html(JSON.stringify(result, null, 2));
+    });
+  });
+
+  $('#debug-mode').click(function() {
+    $('.tile .content, .output-raw').toggleClass('hidden');
+  });
+
+  $('#fire').submit(function(event) {
+    var url = $(document.activeElement).attr('url');
+    var target = $('#coordinates').val();
+    // Don't want to follow the post URL...
+    event.preventDefault();
+
+    // Ajax POST to our API
+    $.post(url, target)
+    .done(function(data) {
+      // Success callback
+    })
+    .fail(function(error) {
+      // Error callback
+    })
+    .always(function() {
+      // Always callback
+      $('#coordinates').val('');
     });
   });
 
   function formatGrid(_result) {
     var ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var board = _result.grid;
     var $output = $('.output');
-    for (var x = 0, xz = _result.length; x < xz; x++) {
+    for (var x = 0, xz = board.length; x < xz; x++) {
       $output.append('<div class="row row-' + x + '"></div>');
-      for (var y = 0, yz = _result[x].length; y < yz; y++) {
-        $('.row-'+x).append('<div class="tile tile-' + ALPHA.charAt(y) + '_' + x + '">' + _result[x][y] + '</div>');
+      for (var y = 0, yz = board[x].length; y < yz; y++) {
+        $('.row-'+x).append('<div class="tile tile-' + ALPHA.charAt(y) + '_' + x + '">' + '<span class="content hidden">' + board[x][y] + "</span>" + '</div>');
       }
     }
   }
